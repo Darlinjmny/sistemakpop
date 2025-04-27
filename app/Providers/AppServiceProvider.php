a3992b859f49a903117bb9d31ab3e171f0b50725
Darlinjmny <161986050+Darlinjmny@users.noreply.github.com>
2025-04-27 15:03:51 -0400

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Paginator::useBootstrap(); 
+        if (app()->environment('production')) { 
+            URL::forceScheme('https'); 
+        }
     }
 }
